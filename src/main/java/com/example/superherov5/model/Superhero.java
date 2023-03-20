6bc4170c20a958e31c1ba8bf59b3c88ae45b9dc2
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2023-03-20 11:35:41 +0000

    test

--- src/main/java/com/example/superherov5/model/Superhero.java
+++ src/main/java/com/example/superherov5/model/Superhero.java
@@ -10,7 +10,7 @@ public class Superhero {
     private int superheroId;
     private String superHeroName;
     private String reelName;
-    private String heoo;
+    private String hallo;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private java.sql.Date creationYear;
 
